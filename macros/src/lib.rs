fdf3e8161ab52959f14a93d22a975d700468118b
zhuy <zhuy>
2024-04-28 15:30:19 +0800

    feat: ParamSetElement和 SystemParam 拆开

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -45,6 +45,16 @@ pub fn derive_system_set(input: TokenStream) -> TokenStream {
     derive_label_inner(input, "SystemSet")
 }
 
+#[proc_macro_derive(Resource)]
+pub fn derive_resource(_input: TokenStream) -> TokenStream {
+    // component::derive_resource(input)
+    TokenStream::from(quote! {})
+}
+
+#[proc_macro_derive(Component, attributes(component))]
+pub fn derive_component(_input: TokenStream) -> TokenStream {
+    TokenStream::from(quote! {})
+}
 /// Implement `SystemParam` to use a struct as a parameter in a system
 #[proc_macro_derive(SystemParam, attributes(system_param))]
 pub fn derive_system_param(input: TokenStream) -> TokenStream {
@@ -163,19 +173,20 @@ pub fn derive_system_param(input: TokenStream) -> TokenStream {
             .predicates
             .push(syn::parse_quote!(#field_type: #path::system::ReadOnlySystemParam));
     }
+    let struct_name = &ast.ident;
 
     let fields_alias =
-        ensure_no_collision(format_ident!("__StructFieldsAlias"), token_stream.clone());
+        ensure_no_collision(format_ident!("{}__StructFieldsAlias", struct_name), token_stream.clone());
 
-    let struct_name = &ast.ident;
+    
     let state_struct_visibility = &ast.vis;
-    let state_struct_name = ensure_no_collision(format_ident!("FetchState"), token_stream);
+    let state_struct_name = ensure_no_collision(format_ident!("{}FetchState", struct_name), token_stream);
 
     TokenStream::from(quote! {
         // We define the FetchState struct in an anonymous scope to avoid polluting the user namespace.
         // The struct can still be accessed via SystemParam::State, e.g. EventReaderState can be accessed via
         // <EventReader<'static, 'static, T> as SystemParam>::State
-        const _: () = {
+        // const _: () = {
             // Allows rebinding the lifetimes of each field type.
             type #fields_alias <'w, #punctuated_generics_no_bounds> = (#(#tuple_types,)*);
 
@@ -239,21 +250,140 @@ pub fn derive_system_param(input: TokenStream) -> TokenStream {
                     unsafe { std::mem::transmute(Self::get_param(world, system_meta, state)) }
                 }
             }
-
-            impl<#punctuated_generics> #path::param_set::ParamSetElement for
-                #struct_name <#(#shadowed_lifetimes,)* #punctuated_generic_idents> #where_clause
-            {
-                fn init_set_state<'w>(world: &'w #path::world::World, system_meta: &'w mut #path::system::SystemMeta) -> Self::State {
-                    #state_struct_name {
-                        state: <#fields_alias::<'_, #punctuated_generic_idents> as #path::param_set::ParamSetElement>::init_set_state(world, system_meta),
-                    }
-                }
-            }
             // Safety: Each field is `ReadOnlySystemParam`, so this can only read from the `World`
             // unsafe impl<'w, 's, #punctuated_generics> #path::system::ReadOnlySystemParam for #struct_name #ty_generics #read_only_where_clause {}
-        };
+        // };
     })
 }
+
+/// Implement `SystemParam` to use a struct as a parameter in a system
+#[proc_macro_derive(ParamSetElement, attributes(param_set_element))]
+pub fn derive_param_set_element(input: TokenStream) -> TokenStream {
+    let token_stream = input.clone();
+    let ast = parse_macro_input!(input as DeriveInput);
+    let syn::Data::Struct(syn::DataStruct {
+        fields: field_definitions,
+        ..
+    }) = ast.data
+    else {
+        return syn::Error::new(
+            ast.span(),
+            "Invalid `SystemParam` type: expected a `struct`",
+        )
+        .into_compile_error()
+        .into();
+    };
+    let path = ecs_path();
+
+    let mut field_locals = Vec::new();
+    let mut fields = Vec::new();
+    let mut field_types = Vec::new();
+    for (i, field) in field_definitions.iter().enumerate() {
+        field_locals.push(format_ident!("f{i}"));
+        let i = Index::from(i);
+        fields.push(
+            field
+                .ident
+                .as_ref()
+                .map(|f| quote! { #f })
+                .unwrap_or_else(|| quote! { #i }),
+        );
+        field_types.push(&field.ty);
+    }
+
+    let generics = ast.generics;
+
+    // Emit an error if there's any unrecognized lifetime names.
+    for lt in generics.lifetimes() {
+        let ident = &lt.lifetime.ident;
+        let w = format_ident!("w");
+        let s = format_ident!("s");
+        if ident != &w && ident != &s {
+            return syn::Error::new_spanned(
+                lt,
+                r#"invalid lifetime name: expected `'w` or `'s`
+ 'w -- refers to data stored in the World.
+ 's -- refers to data stored in the SystemParam's state.'"#,
+            )
+            .into_compile_error()
+            .into();
+        }
+    }
+
+    let (_impl_generics, _ty_generics, where_clause) = generics.split_for_impl();
+
+    let lifetimeless_generics: Vec<_> = generics
+        .params
+        .iter()
+        .filter(|g| !matches!(g, GenericParam::Lifetime(_)))
+        .collect();
+
+    let shadowed_lifetimes: Vec<_> = generics.lifetimes().map(|_| quote!('_)).collect();
+
+    let mut punctuated_generics = Punctuated::<_, Comma>::new();
+    punctuated_generics.extend(lifetimeless_generics.iter().map(|g| match g {
+        GenericParam::Type(g) => GenericParam::Type(TypeParam {
+            default: None,
+            ..g.clone()
+        }),
+        GenericParam::Const(g) => GenericParam::Const(ConstParam {
+            default: None,
+            ..g.clone()
+        }),
+        _ => unreachable!(),
+    }));
+
+    let mut punctuated_generic_idents = Punctuated::<_, Comma>::new();
+    punctuated_generic_idents.extend(lifetimeless_generics.iter().map(|g| match g {
+        GenericParam::Type(g) => &g.ident,
+        GenericParam::Const(g) => &g.ident,
+        _ => unreachable!(),
+    }));
+
+    let mut tuple_types: Vec<_> = field_types.iter().map(|x| quote! { #x }).collect();
+    let mut tuple_patterns: Vec<_> = field_locals.iter().map(|x| quote! { #x }).collect();
+
+    // If the number of fields exceeds the 16-parameter limit,
+    // fold the fields into tuples of tuples until we are below the limit.
+    const LIMIT: usize = 16;
+    while tuple_types.len() > LIMIT {
+        let end = Vec::from_iter(tuple_types.drain(..LIMIT));
+        tuple_types.push(parse_quote!( (#(#end,)*) ));
+
+        let end = Vec::from_iter(tuple_patterns.drain(..LIMIT));
+        tuple_patterns.push(parse_quote!( (#(#end,)*) ));
+    }
+
+    // Create a where clause for the `ReadOnlySystemParam` impl.
+    // Ensure that each field implements `ReadOnlySystemParam`.
+    let mut read_only_generics = generics.clone();
+    let read_only_where_clause = read_only_generics.make_where_clause();
+    for field_type in &field_types {
+        read_only_where_clause
+            .predicates
+            .push(syn::parse_quote!(#field_type: #path::system::ReadOnlySystemParam));
+    }
+    let struct_name = &ast.ident;
+    // let r = struct_name.to_string();
+    let fields_alias =
+        ensure_no_collision(format_ident!("{}__StructFieldsAlias", struct_name), token_stream.clone());
+   
+    // let state_struct_visibility = &ast.vis;
+    let state_struct_name = ensure_no_collision(format_ident!("{}FetchState", struct_name), token_stream);
+
+    TokenStream::from(quote! {
+        impl<#punctuated_generics> #path::param_set::ParamSetElement for
+            #struct_name <#(#shadowed_lifetimes,)* #punctuated_generic_idents> #where_clause
+        {
+            fn init_set_state<'w>(world: &'w #path::world::World, system_meta: &'w mut #path::system::SystemMeta) -> Self::State {
+                #state_struct_name {
+                    state: <#fields_alias::<'_, #punctuated_generic_idents> as #path::param_set::ParamSetElement>::init_set_state(world, system_meta),
+                }
+            }
+        }
+    })
+}
+
 
 #[proc_macro_derive(Bundle, attributes(bundle))]
 pub fn derive_bundle(input: TokenStream) -> TokenStream {
