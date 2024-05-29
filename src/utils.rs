dc60ac298884f072796891986258b89c3c84cca6
wzjsun <530739162@qq.com>
2024-05-29 17:47:59 +0800

    fix

--- src/utils.rs
+++ src/utils.rs
@@ -7,25 +7,12 @@ pub trait VecExt<T> {
 }
 
 impl<T: Null> VecExt<T> for Vec<T> {
-    default fn insert_value(&mut self, index: usize, value: T) {
-        if size_of::<T>() > 0 {
-            return self.insert(index, value);
-        }
+    fn insert_value(&mut self, index: usize, value: T) {
+        // if size_of::<T>() > 0 {
+        //     return self.insert(index, value);
+        // }
         if index >= self.len() {
             self.resize_with(index + 1, || T::null());
-        }
-        unsafe { *self.get_unchecked_mut(index) = value; };
-    }
-}
-impl<T: Null + Clone> VecExt<T> for Vec<T> {
-    fn insert_value(&mut self, index: usize, value: T) {
-        if size_of::<T>() > 0 {
-            return self.insert(index, value);
-        }
-        let t = T::null();
-        if index >= self.len() {
-            // println!("{:p}: index + 1: {}", self, index + 1);
-            self.resize(index + 1, t);
         }
         unsafe { *self.get_unchecked_mut(index) = value; };
     }
