54e1203be8be8f53fbbda684bd9bc0e3d01c33fd
zmax <zmythleo@gmail.com>
2024-05-29 15:40:31 +0800

    fix ZST

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 use pi_null::Null;
 
 pub trait VecExt<T> {
@@ -6,6 +8,9 @@ pub trait VecExt<T> {
 
 impl<T: Null> VecExt<T> for Vec<T> {
     default fn insert_value(&mut self, index: usize, value: T) {
+        if size_of::<T>() > 0 {
+            return self.insert(index, value);
+        }
         if index >= self.len() {
             self.resize_with(index + 1, || T::null());
         }
@@ -14,6 +19,9 @@ impl<T: Null> VecExt<T> for Vec<T> {
 }
 impl<T: Null + Clone> VecExt<T> for Vec<T> {
     fn insert_value(&mut self, index: usize, value: T) {
+        if size_of::<T>() > 0 {
+            return self.insert(index, value);
+        }
         let t = T::null();
         if index >= self.len() {
             // println!("{:p}: index + 1: {}", self, index + 1);
